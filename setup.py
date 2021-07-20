3e4ac614f996c2013a974c01c04d99559983f584
Azizmurod <murodovazizmurod@gmail.com>
2021-07-20 17:56:55 +0500

    First Release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'ormx',
   packages = ['ormx', 'ORMX'],
-  version = '0.1',
+  version = '0.1.2',
   license='MIT',
   description = 'Simple Orm package',
   author = 'Murodov Azizmurod',
