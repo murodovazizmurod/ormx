b92cddaeffb76d77e763e0e324166ab459d963e9
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-07-22 19:40:43 +0500

    🧪Created simple tests

---
+++ test/core.py
@@ -0,0 +1,54 @@
+import pytest
+
+from ormx import Database
+from ormx.models import (
+    Table,
+    Column,
+    ForeignKey
+)
+
+db = Database("data.db")
+
+
+class Author(Table):
+    name = Column(str)
+    age = Column(int)
+
+
+class Post(Table):
+    title = Column(str)
+    draft = Column(bool)
+    author = ForeignKey(Author)
+
+
+db.create(Author)
+db.create(Post)
+
+db.save(Author(name="Linus", age=99))
+
+db.save(Post(title="Programming",
+             draft=False,
+             author=db.get(Author, 1)))
+
+test_post = db.get(Post, 1)
+test_author = db.get(Author, 1)
+
+
+def test_authors():
+    assert len(db.all(Author)) > 0
+
+
+def test_posts():
+    assert len(db.all(Post)) > 0
+
+
+def test_author_name():
+    assert 'Linus' == test_author.name
+
+
+def test_post_title():
+    assert "Programming" == test_post.title
+
+
+def test_post_author():
+    assert "Linus" == test_post.author.name
